f65e4168442ef47e07a140315ddb83c18c680b17
Yuan Fang <42786261+yuanfang313@users.noreply.github.com>
2020-08-06 23:05:01 -0400

    initial commit

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -186,27 +186,31 @@ for(var i = 0; i < boxes.length; i++) {
   boxes[i].textContent = 'I changed to blue!';
 }
 */
-for (const cur of boxesArr6) {
-  if(cur.className.includes('blue')){
-    continue;
-  }
-  cur.textContent = 'I changed to blue!';
-}
+// for (const cur of boxesArr6) {
+//   if(cur.className.includes('blue')){
+//     continue;
+//   }
+//   cur.textContent = 'I changed to blue!';
+// }
 
-// ES5
-var ages = [12, 17, 8,21, 14, 11];
+// // ES5
+// var ages = [12, 17, 8,21, 14, 11];
 
-var full = ages.map(function(cur){
- return cur >= 18;
-});
-console.log(full);
+// var full = ages.map(function(cur){
+//  return cur >= 18;
+// });
+// console.log(full);
 
-console.log(full.indexOf(true));
-console.log(ages[full.indexOf(true)]);
+// console.log(full.indexOf(true));
+// console.log(ages[full.indexOf(true)]);
 
-// ES6
-console.log(ages.findIndex(cur => cur >= 18));
-console.log(ages.find(cur => cur >= 18));
+// // ES6
+// console.log(ages.findIndex(cur => cur >= 18));
+// console.log(ages.find(cur => cur >= 18));
+
+// Lecture: Spread operator
+
+
 
 
 
