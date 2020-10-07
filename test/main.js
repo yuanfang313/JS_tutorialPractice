6a9dea292b7c111d1eac10e057e1989e68ee13b5
Yuan Fang <yxf313@miami.edu>
2020-10-07 14:35:33 -0400

    update

--- test/main.js
+++ test/main.js
@@ -1,77 +1,28 @@
-class Node {
-    constructor(val) {
-        this.left = null;
-        this.right = null;
-        this.val = val;
-    }
-}
+const walkArr = ['w']
 
-Node.prototype.addNode = function(n) {
-    if (n.val < this.val) {
-        if (this.left == null) {
-            this.left = n;
-        } else {
-            this.left.addNode(n);
+function isValidWalk(walk) {
+    let xSum = 0;
+    let ySum = 0;
+    for (step of walk) {
+        switch (step) {
+            case 'n':
+                ySum++;
+                break;
+            case 's':
+                ySum--;
+                break;
+            case 'e':
+                xSum++;
+                break;
+            case 'w':
+                xSum--;
+                break;
         }
-    } else if (n.val > this.val) {
-        if (this.right == null) {
-            this.right = n;
-        } else {
-            this.right.addNode(n)
-        }
     }
+    console.log(xSum, ySum);
+    if (xSum === 0 && ySum === 0) return true;
+
+    return false;
 }
 
-Node.prototype.visit = function() {
-    if (this.left != null) {
-        this.left.visit();
-    }
-    console.log(this.val);
-    if (this.right != null) {
-        this.right.visit();
-    }  
-}
-
-class BST {
-    constructor() {
-        this.root = null;
-    }
-}
-
-BST.prototype.addVal = function(val) {
-    let n = new Node(val);
-    if (this.root == null) {
-        this.root = n;
-    } else {
-        this.root.addNode(n);
-    }
-}
-
-BST.prototype.traverse = function() {
-    this.root.visit();
-}
-
-
-
-let tree = new BST();
-
-for (let i = 0; i < 10; i++) {
-    tree.addVal(Math.floor(Math.random() * 100));
-}
-tree.traverse();
-
-const isBalanced = root => {
-    if (!root) return true;
-
-    return isBalanced(root.left)
-}
-let stack = [tree];
-//stack.pop();
-
-
-const res = isBalanced(tree);
-console.log(tree);
-console.log(stack.length);
-console.log(res);
-
-
+console.log(isValidWalk(walkArr));
